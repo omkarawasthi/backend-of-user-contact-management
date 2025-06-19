6db9fe1ed8e6be1a42f9a846290cb91c3f286f9d
omkarawasthi <omkarawasthi67@gmail.com>
2025-06-19 15:05:24 +0530

    commited

--- usermanagement/usermanagement/settings.py
+++ usermanagement/usermanagement/settings.py
@@ -100,8 +100,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DATABASE_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('PORT'),
     }
 }
 
