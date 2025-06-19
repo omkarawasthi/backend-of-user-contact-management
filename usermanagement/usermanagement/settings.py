55490eebaadbaa704e5191a0b14a240c15cebba1
omkarawasthi <omkarawasthi67@gmail.com>
2025-06-19 15:40:25 +0530

    commited

--- usermanagement/usermanagement/settings.py
+++ usermanagement/usermanagement/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'backend-of-user-contact-management.onrender.com',
     "https://frontend-user-contact-management-pr-steel.vercel.app",
 ]
 
